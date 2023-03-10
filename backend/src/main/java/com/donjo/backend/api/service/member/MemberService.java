7fa20a7bd8c2b641e4983bea52d8da0485b25bc6
이재욱 <jaewook_lee@outlook.com>
2023-03-10 11:35:17 +0900

    :white_check_mark: test: web3j 연결 테스트 코드 작성

--- backend/src/main/java/com/donjo/backend/api/service/member/MemberService.java
+++
@@ -1,7 +0,0 @@
-package com.donjo.backend.api.service.member;
-
-import java.security.SignatureException;
-
-public interface MemberService {
-    boolean checkSignature(String address, String signature) throws SignatureException;
-}
