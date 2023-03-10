1c8cf9d55bd3c90135b0b66517d6795b43adc763
송현주 <shyunju7@naver.com>
2023-03-10 14:38:54 +0900

    ✨ feat: store slice 설정(donation, items, member, wishlist)

---
+++ frontend/don-jo-app/src/stores/member/index.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+export const memberSlice = createSlice({
+  name: "member",
+  initialState: {},
+  reducers: {},
+});
+
+export const {} = memberSlice.actions;
+export default memberSlice.reducer;
