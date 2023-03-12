540292186dd9b709004bcb17f1748e283357d49b
안태현 <dksxogus1012@gmail.com>
2023-03-13 04:46:11 +0900

    :sparkles: feat: Header 설정 & style 파일 추가

---
+++ frontend/don-jo-app/src/components/Common/Header/style.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 4rem;
+
+  background-color: var(--color-background);
+
+  display: flex;
+  align-items: center;
+
+  box-sizing: border-box;
+  padding: 0 1.5rem;
+`;
+
+export const Logo = styled.div`
+  width: 2.5rem;
+  height: 2.5rem;
+
+  background-color: var(--color-primary);
+`;
