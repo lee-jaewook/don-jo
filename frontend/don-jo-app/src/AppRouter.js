d7361492d7dfb3b8a95551a9ac5c084b8accdc96
Hyunsu <83157338+hyunssu@users.noreply.github.com>
2023-03-13 17:31:23 +0900

    :zap: update : 라우터 파일에 메타마스크 연결 코드 추가
    메타마스크 연결 시 추가적인  고려사항 적용 중

--- frontend/don-jo-app/src/AppRouter.js
+++ frontend/don-jo-app/src/AppRouter.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Intro from "./pages/Intro";
 import Guide from "./pages/Guide";
@@ -6,10 +6,73 @@ import GuideDetail from "./pages/GuideDetail";
 import Personal from "./pages/Personal";
 import DashBoard from "./pages/DashBoard";
 import Error from "./pages/Error";
+import { useDispatch, useSelector } from "react-redux";
+import Web3 from "web3";
+import { setWeb3 } from "./stores/member";
 
 const AppRouter = () => {
-  // 비로그인 상태인 경우(isLogin 임시 처리)
-  let isLogin = false;
+  const member = useSelector((state) => state.member);
+  // const dispatch = useDispatch();
+  // // Metamask Login이 안돼 있다면
+
+  // const handleAccountsChanged = (accounts) => {
+  //   if (accounts.length === 0) {
+  //   } else {
+  //   }
+  // };
+
+  // const connectWallet = async () => {
+  //   if (typeof window.ethereum !== "undefined") {
+  //     // 메타마스크 설치되어 있으면, 로그인 요청
+  //     try {
+  //       await window.ethereum.request({ method: "eth_requestAccounts" });
+  //       console.log("MetaMask is connected");
+  //     } catch (error) {
+  //       // 메타마스크가 설치는 되어 있지만, 로그인은 안된 상태.
+  //     }
+  //   }
+
+  //   try {
+  //     const web3 = new Web3(window.ethereum);
+  //     web3.eth.net.getId().then((chainId) => {
+  //       const infuraWeb3 = new Web3(
+  //         new Web3.providers.HttpProvider(
+  //           "https://sepolia.infura.io/v3/1d3e75e17f6f49fea625e1d555738da0"
+  //         )
+  //       );
+  //       web3.setProvider(infuraWeb3.currentProvider);
+  //       dispatch(setWeb3(web3));
+  //       window.ethereum.on("accountsChanged", handleAccountsChanged);
+  //     });
+  //   } catch {
+  //     console.log("error!!!");
+  //   }
+  // };
+  // useEffect(() => {
+  //   connectWallet();
+  // }, [member.web3]);
+
+  // useEffect(() => {
+  //   if (typeof window.ethereum !== "undefined") {
+  //     // 이벤트 리스너 등록
+  //     window.ethereum.on("accountsChanged", handleAccountsChanged);
+
+  //     // cleanup 함수에서 이벤트 리스너 제거
+  //     return () => {
+  //       window.ethereum.off("accountsChanged", handleAccountsChanged);
+  //     };
+  //   }
+  // }, []);
+
+  // if (typeof window.ethereum !== "undefined") {
+  //   connectWallet();
+  // } else {
+  //   console.log("이미 로그인돼있음");
+  // }
+  // 로그인 여부 체크
+  let isLogin = member.isLogin;
+  // console.log(member);
+
   if (!isLogin) {
     return (
       <Router>
