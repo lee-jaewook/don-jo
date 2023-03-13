192f05b5d2dc4738b8bd3d29fe1cfb4684cd272d
Hyunsu <83157338+hyunssu@users.noreply.github.com>
2023-03-13 14:35:21 +0900

    :zap: update : update donation contract.
    donation contract 수정.

--- contract/contracts/Donation.sol
+++ contract/contracts/Donation.sol
@@ -7,6 +7,7 @@ contract Donation {
 
   struct Member {
     address donor;
+    string nickName;
     uint256 amount;
     uint256 time;
   }
@@ -22,8 +23,7 @@ contract Donation {
     string donationType;
     string title;
     Member[] donationInfo;
-  }
-    
+  } 
 
   mapping(address => DonationData) public donations;
   mapping(address => mapping(uint16 => DonationData)) public wishlist;
@@ -43,10 +43,10 @@ contract Donation {
     
     // 할당받은 주소가 없다면 주소를 할당해준다.
     DonationData memory myWallet = DonationData({
-      title: "",
+      index: 0,
       amount: 0,
       owner: _to,
-      index: 0,
+      title: "donation",
       donationType: "donation",
       donationInfo: new Member[](0),
       effectiveness: true
@@ -55,34 +55,63 @@ contract Donation {
     donations[_to] = myWallet;
   }
   
-  // 1. 기부하기
+  // 필요한 함수
+  // 1. 기부하기(donation, wishlist) => 중복 가능.
+  // 2. 구매하기(items) => 중복 불가능.
+  // 3. 정보 가져오기
+  //     3-1. 기부 내역, 총 거래량, 현재
+  //     3-2. wistlist 후원내역
+  //     3-3. items 구매내역
+
+
+  // 문자열 비교 함수
+  function compareStrings(string memory a, string memory b) public pure returns (bool) {
+    return keccak256(abi.encode(a)) == keccak256(abi.encodePacked(b));
+  }
+
+  // 1. 기부하기(donation, wishlist) => 중복 가능
+  function donate(string memory donationType, uint16 idx) public payable {
+    // 주소, donationType만 있으면 되고,
+    require(msg.value > 0, "You must send a positive amount.");
+    if(compareStrings(donationType, "donation")) {
+      donations[msg.sender].amount += msg.value;
+      donations[msg.sender].donationInfo.push();
+    }
+    else if (compareStrings(donationType, "wishlist")) {
+      wishlist[msg.sender][idx].amount += msg.value;
+      wishlist[msg.sender][idx].donationInfo.push();
+    } 
+    else {
+      revert("Invalid donation type.");
+    }
+    // receiver.transfer(msg.value);
+    // emit DonationReceived(msg.sender, msg.value, donationType);
+  }
+
+  // 2. items 구매하기 => 중복 불가, 구매한 목록인지 확인 필요.
+  function getItem() public payable {
+
+  }
+  // 3. 
   // 2. 지갑에서 ETH 인출하기
   // 3. 지갑에서 ETH 정보 가져오기
   // 4. 지갑에서 wishList, items 정보 가져오기...
 
-
-
   // 지갑에서 ETH정보 가져오기
   // 0
-
-
+  
   // 지갑에서 ETH 인출하기
   
   // 기부하기..
-  function donate(string memory donationType) public payable {
-      require(msg.value > 0, "You must send a positive amount.");
-      donations[msg.sender][donationType] += msg.value;
-      emit DonationReceived(msg.sender, msg.value, donationType);
-  }
+
   
   function withdraw(uint256 amount) public {
-      require(msg.sender == owner, "Only the contract owner can withdraw funds.");
-      require(amount <= address(this).balance, "Insufficient funds in the contract.");
-      owner.transfer(amount);
+    require(msg.sender == owner, "Only the contract owner can withdraw funds.");
+    require(amount <= address(this).balance, "Insufficient funds in the contract.");
+    owner.transfer(amount);
   }
 
   function getDonationAmount(address donor, string memory donationType) public view returns(uint256) {
-      return donations[donor][donationType];
-  }
- 
+    return donations[donor][donationType];
+  } 
 }
