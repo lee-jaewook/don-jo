cf0ad251a5c354c83cb2bfd86b456f6e6a8665ed
안태현 <dksxogus1012@gmail.com>
2023-03-13 13:02:59 +0900

    :sparkles: feat: SelectBox 설정 & style 파일 추가

---
+++ frontend/don-jo-app/src/components/Common/SelectBox/style.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const SelectBox = styled.div`
+  position: relative;
+  width: 200px;
+  padding: 8px;
+  border-radius: 12px;
+  background-color: #ffffff;
+  align-self: center;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  cursor: pointer;
+  &::before {
+    content: "⌵";
+    position: absolute;
+    top: 1px;
+    right: 8px;
+    color: #49c181;
+    font-size: 20px;
+  }
+`;
+export const Label = styled.label`
+  font-size: 14px;
+  margin-left: 4px;
+  text-align: center;
+`;
+export const SelectOptions = styled.ul`
+  position: absolute;
+  list-style: none;
+  top: 18px;
+  left: 0;
+  width: 100%;
+  overflow: hidden;
+  height: 90px;
+  max-height: ${(props) => (props.show ? "none" : "0")};
+  padding: 0;
+  border-radius: 8px;
+  background-color: #222222;
+  color: #fefefe;
+`;
+export const Option = styled.li`
+  font-size: 14px;
+  padding: 6px 8px;
+  transition: background-color 0.2s ease-in;
+  &:hover {
+    background-color: #595959;
+  }
+`;
