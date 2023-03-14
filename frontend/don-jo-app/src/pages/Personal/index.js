e88bb9a2c6e065e68f699c01d839a46474a16d37
안태현 <dksxogus1012@gmail.com>
2023-03-14 11:09:57 +0900

    :bug: fix: 파일명 소문자로 수정 & BasicModal children PropType 수정 & SelectBox 드롭다운 boolean 변수명 변경 & SelectBox 열고닫는 함수 형태 변경

--- frontend/don-jo-app/src/pages/Personal/index.js
+++ frontend/don-jo-app/src/pages/Personal/index.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { BasicModal } from "../../components/Common/Modal/BasicModal";
 
 const Personal = () => {
-  return <div>Personal Page</div>;
+  // Test Code Start
+  const [isShowModal, setIsShowModal] = useState(false);
+
+  const hey = <div>호롤롤로</div>;
+  //Test Code End
+
+  return (
+    <div>
+      Personal Page
+      {/* Test Code Start*/}
+      <div>
+        <button onClick={() => setIsShowModal(true)}>베이직 모달 버튼</button>
+      </div>
+      {isShowModal && (
+        <BasicModal handleSetShowModal={setIsShowModal} children={hey} />
+      )}
+      {/* Test Code End */}
+    </div>
+  );
 };
 
 export default Personal;
