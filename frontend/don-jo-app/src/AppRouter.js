ba158394276bd0e733108dbe33704c6d13cf23c1
송현주 <shyunju7@naver.com>
2023-03-14 13:28:17 +0900

    ✨ feat: dashboard 세부 페이지 설정

--- frontend/don-jo-app/src/AppRouter.js
+++ frontend/don-jo-app/src/AppRouter.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  redirect,
+  Navigate,
+} from "react-router-dom";
 import Intro from "./pages/Intro";
 import Guide from "./pages/Guide";
 import GuideDetail from "./pages/GuideDetail";
@@ -9,7 +15,7 @@ import Error from "./pages/Error";
 
 const AppRouter = () => {
   // 비로그인 상태인 경우(isLogin 임시 처리)
-  let isLogin = false;
+  let isLogin = true;
   if (!isLogin) {
     return (
       <Router>
@@ -31,7 +37,11 @@ const AppRouter = () => {
         <Route path="/" element={<Intro />} />
         <Route path="/guide" element={<Guide />} />
         <Route path="/guide/:title" element={<GuideDetail />} />
-        <Route path="/dashboard" element={<DashBoard />} />
+        <Route
+          path="/dashboard"
+          element={<Navigate replace to="/dashboard/home" />}
+        />
+        <Route path="/dashboard/:category" element={<DashBoard />} />
         <Route path="/:pageName" element={<Personal />} />
         <Route path="*" element={<Error />} />
       </Routes>
