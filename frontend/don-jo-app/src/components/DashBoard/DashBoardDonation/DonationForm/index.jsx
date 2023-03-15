ed9a5329cfec3ddff8d4b2b0ee2b4a3a7c9a1623
송현주 <shyunju7@naver.com>
2023-03-15 12:53:08 +0900

    ✨ feat: DonationForm 구현 및 emoji picker 추가 & UI 적용

--- frontend/don-jo-app/src/components/DashBoard/DashBoardDonation/DonationForm/index.jsx
+++ frontend/don-jo-app/src/components/DashBoard/DashBoardDonation/DonationForm/index.jsx
@@ -1,13 +1,125 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useInput } from "../../../../hooks/useInput";
 import BasicTitle from "../../../Common/BasicTitle";
+import EmojiPicker from "emoji-picker-react";
+import { FiChevronDown } from "react-icons/fi";
+import * as S from "./style";
+import BasicInput from "../../../Common/BasicInput";
+import BasicButton from "../../../Common/BasicButton";
 const DonationForm = () => {
-  const handleOnClickEditEmoji = () => {};
+  const [selectedEmoji, setSelectedEmoji] = useState("💕"); // user별 default emoji 설정
+  const [emojiName, onChangeEmojiName] = useInput("Heart"); // user별 default emoji 이름 설정
+  const [currentPrice, onChangeCurrentPrice] = useInput("1"); //// user별 default 가격 설정
+  const [isShowEmojiPicker, setShowEmojiPicker] = useState(false);
+  const handleSetShowEmojiPicker = () => setShowEmojiPicker((prev) => !prev);
+  const handleOnClickEmoji = (item) => {
+    console.log(item);
+    setSelectedEmoji(item.emoji);
+    onChangeEmojiName(item.names[0]);
+    setShowEmojiPicker(false);
+  };
+
+  const handleOnClickButton = () => {
+    console.log("save donation settings");
+  };
+
+  useEffect(() => {
+    console.log(currentPrice);
+  }, [currentPrice]);
+
   return (
-    <div>
+    <S.FormWrapper>
       <BasicTitle text="Choose your Emoji" />
-      <p>Replace “cookie” with anything you like.</p>
-      <button onClick={handleOnClickEditEmoji}>이모지박스</button>
-    </div>
+      <S.FormDescription>
+        Replace "{emojiName}" with anything you like.
+      </S.FormDescription>
+      <S.EmojiSettingWrapper>
+        <S.EmojiButton onClick={handleSetShowEmojiPicker}>
+          {selectedEmoji}
+          <FiChevronDown size="16px" />
+        </S.EmojiButton>
+        <BasicInput
+          type="text"
+          value={emojiName}
+          handleOnChangeValue={onChangeEmojiName}
+        />
+        {isShowEmojiPicker && (
+          <S.EmojiPickerModal>
+            <EmojiPicker onEmojiClick={handleOnClickEmoji} />
+          </S.EmojiPickerModal>
+        )}
+      </S.EmojiSettingWrapper>
+
+      <BasicTitle text="Price per Donation" />
+      <S.FormDescription>
+        Change the default price of a coffee to an amount of your choice.
+      </S.FormDescription>
+
+      <S.RadioGroup>
+        <S.RadioWrapper>
+          <S.RadioButton
+            id="dollar1"
+            type="radio"
+            name="price"
+            value="1"
+            onChange={onChangeCurrentPrice}
+            defaultChecked
+          />
+          <S.RadioLabel htmlFor="dollar1">$1</S.RadioLabel>
+        </S.RadioWrapper>
+        <S.RadioWrapper>
+          <S.RadioButton
+            id="dollar2"
+            type="radio"
+            name="price"
+            value="2"
+            onChange={onChangeCurrentPrice}
+          />
+          <S.RadioLabel htmlFor="dollar2">$2</S.RadioLabel>
+        </S.RadioWrapper>
+        <S.RadioWrapper>
+          <S.RadioButton
+            id="dollar3"
+            type="radio"
+            name="price"
+            value="3"
+            onChange={onChangeCurrentPrice}
+          />
+          <S.RadioLabel htmlFor="dollar3">$3</S.RadioLabel>
+        </S.RadioWrapper>
+        <S.RadioWrapper>
+          <S.RadioButton
+            id="dollar4"
+            type="radio"
+            name="price"
+            value="4"
+            onChange={onChangeCurrentPrice}
+          />
+          <S.RadioLabel htmlFor="dollar4">$4</S.RadioLabel>
+        </S.RadioWrapper>
+        <S.RadioWrapper>
+          <S.RadioButton
+            id="dollar5"
+            type="radio"
+            name="price"
+            value="5"
+            onChange={onChangeCurrentPrice}
+          />
+          <S.RadioLabel htmlFor="dollar5">$5</S.RadioLabel>
+        </S.RadioWrapper>
+      </S.RadioGroup>
+      <BasicTitle text="Thank you message" />
+      <S.FormDescription>
+        This will be visible after the payment and in the receipt email. <br />
+        Write a personable thank you message, and include any rewards if you
+        like.
+      </S.FormDescription>
+      <BasicInput />
+
+      <S.ButtonWrapper>
+        <BasicButton text="Save" handleOnClickButton={handleOnClickButton} />
+      </S.ButtonWrapper>
+    </S.FormWrapper>
   );
 };
 
