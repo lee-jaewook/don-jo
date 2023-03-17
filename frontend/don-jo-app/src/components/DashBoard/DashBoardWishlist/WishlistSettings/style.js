060a311e4c5423d1198ae0ecd017c332efc43259
송현주 <shyunju7@naver.com>
2023-03-17 16:10:17 +0900

    💄 style: dashboard wishlist UI 적용

--- frontend/don-jo-app/src/components/DashBoard/DashBoardWishlist/WishlistSettings/style.js
+++ frontend/don-jo-app/src/components/DashBoard/DashBoardWishlist/WishlistSettings/style.js
@@ -8,3 +8,22 @@ export const SettingWrapper = styled.div`
   padding: 2rem 1.5rem;
   margin-bottom: 2.5rem;
 `;
+export const AddButton = styled.button`
+  width: 25.25rem;
+  height: 12.25rem;
+  max-width: 50.75rem;
+  border: 2px dashed #d2d2d2;
+  border-radius: 1.25rem;
+  margin-bottom: 2.5rem;
+`;
+
+export const AddIcon = styled.div`
+  display: flex;
+  width: 3.75rem;
+  height: 3.75rem;
+  border-radius: 50%;
+  background-color: var(--color-text);
+  margin: 0 auto;
+  align-items: center;
+  justify-content: center;
+`;
