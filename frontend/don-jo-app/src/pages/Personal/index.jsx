b8158ed6a0e47f43cc4f878a6f06d404e6fc5df6
안태현 <dksxogus1012@gmail.com>
2023-03-19 22:06:41 +0900

    :sparkles: feat: 자기소개 수정 및 작성 모달 생성

--- frontend/don-jo-app/src/pages/Personal/index.jsx
+++ frontend/don-jo-app/src/pages/Personal/index.jsx
@@ -3,6 +3,8 @@ import { FiEdit } from "react-icons/fi";
 import ExternalLink from "../../components/Personal/ExternalLink";
 import { useState } from "react";
 import PersonalContent from "../../components/Personal/PersonalContent";
+import FullScreenModal from "../../components/Common/Modal/FullScreenModal";
+import IntroductionEdit from "../../components/Personal/IntroductionEdit";
 
 const Personal = () => {
   //로그인 유저 더미 데이터
@@ -33,66 +35,81 @@ const Personal = () => {
 
   const [isBackgroundHover, setIsBackgroundHover] = useState(false);
   const [isProfileHover, setIsProfileHover] = useState(false);
+  const [isShowIntroductionEdit, setIsShowIntroductionEdit] = useState(false);
 
   return (
-    <S.Container>
-      <S.BackgroundImg
-        src={pageOwner.backgroundImgPath}
-        onMouseOver={() => setIsBackgroundHover(true)}
-        onMouseOut={() => setIsBackgroundHover(false)}
-      >
-        {loginUser.memberAddress === pageOwner.memberAddress &&
-          isBackgroundHover && (
-            <S.BackgroundImgEdit>
-              <S.EditIcon>
-                <FiEdit color="white" size={20.35} />
-              </S.EditIcon>
-            </S.BackgroundImgEdit>
-          )}
-      </S.BackgroundImg>
-      <S.Wrapper>
-        <S.ProfileImgContainer>
-          <S.ProfileImg
-            src={pageOwner.profileImgPath}
-            onMouseOver={() => setIsProfileHover(true)}
-            onMouseOut={() => setIsProfileHover(false)}
-          >
-            {loginUser.memberAddress === pageOwner.memberAddress &&
-              isProfileHover && (
-                <S.ProfileImgEdit>
-                  <S.EditIcon>
-                    <FiEdit color="white" size={20.35} />
-                  </S.EditIcon>
-                </S.ProfileImgEdit>
-              )}
-          </S.ProfileImg>
-        </S.ProfileImgContainer>
-        <S.ContentsContainer>
-          <S.UserInfo>
-            <S.Nickname>{pageOwner.nickname}</S.Nickname>
-            <S.SupporterContainer>
-              <S.NumSupporter>
-                {pageOwner.numSupporters
-                  .toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-              </S.NumSupporter>
-              supporter
-            </S.SupporterContainer>
-            <ExternalLink socialList={pageOwner.socialList} />
-            <S.IntroductionContainer>
-              {/* 로그인한 유저와 페이지 주인이 같다면 edit 버튼 표시 */}
-              {loginUser.memberAddress === pageOwner.memberAddress && (
-                <S.IntroductionEdit>
-                  <FiEdit style={{ cursor: "pointer" }} />
-                </S.IntroductionEdit>
-              )}
-              <S.Introduction>{pageOwner.introduction}</S.Introduction>
-            </S.IntroductionContainer>
-          </S.UserInfo>
-          <PersonalContent />
-        </S.ContentsContainer>
-      </S.Wrapper>
-    </S.Container>
+    <div>
+      <S.Container>
+        <S.BackgroundImg
+          src={pageOwner.backgroundImgPath}
+          onMouseOver={() => setIsBackgroundHover(true)}
+          onMouseOut={() => setIsBackgroundHover(false)}
+        >
+          {loginUser.memberAddress === pageOwner.memberAddress &&
+            isBackgroundHover && (
+              <S.BackgroundImgEdit>
+                <S.EditIcon>
+                  <FiEdit color="white" size={20.35} />
+                </S.EditIcon>
+              </S.BackgroundImgEdit>
+            )}
+        </S.BackgroundImg>
+        <S.Wrapper>
+          <S.ProfileImgContainer>
+            <S.ProfileImg
+              src={pageOwner.profileImgPath}
+              onMouseOver={() => setIsProfileHover(true)}
+              onMouseOut={() => setIsProfileHover(false)}
+            >
+              {loginUser.memberAddress === pageOwner.memberAddress &&
+                isProfileHover && (
+                  <S.ProfileImgEdit>
+                    <S.EditIcon>
+                      <FiEdit color="white" size={20.35} />
+                    </S.EditIcon>
+                  </S.ProfileImgEdit>
+                )}
+            </S.ProfileImg>
+          </S.ProfileImgContainer>
+          <S.ContentsContainer>
+            <S.UserInfo>
+              <S.Nickname>{pageOwner.nickname}</S.Nickname>
+              <S.SupporterContainer>
+                <S.NumSupporter>
+                  {pageOwner.numSupporters
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                </S.NumSupporter>
+                supporter
+              </S.SupporterContainer>
+              <ExternalLink socialList={pageOwner.socialList} />
+              <S.IntroductionContainer>
+                {/* 로그인한 유저와 페이지 주인이 같다면 edit 버튼 표시 */}
+                {loginUser.memberAddress === pageOwner.memberAddress && (
+                  <S.IntroductionEdit
+                    onClick={() => {
+                      setIsShowIntroductionEdit(true);
+                    }}
+                  >
+                    <FiEdit style={{ cursor: "pointer" }} />
+                  </S.IntroductionEdit>
+                )}
+                <S.Introduction>{pageOwner.introduction}</S.Introduction>
+              </S.IntroductionContainer>
+            </S.UserInfo>
+            <PersonalContent />
+          </S.ContentsContainer>
+        </S.Wrapper>
+      </S.Container>
+      {isShowIntroductionEdit && (
+        <FullScreenModal
+          handleSetShowModal={setIsShowIntroductionEdit}
+          children={
+            <IntroductionEdit handleSetShowModal={setIsShowIntroductionEdit} />
+          }
+        />
+      )}
+    </div>
   );
 };
 
