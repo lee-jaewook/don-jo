4cefd0c24543635506935da58daa175298b6da5d
송현주 <shyunju7@naver.com>
2023-03-19 17:15:41 +0900

    💄 style: dashboard 통계 카드 및 목록 반응형 UI 적용

--- frontend/don-jo-app/src/components/DashBoard/DashBoardCard/style.js
+++ frontend/don-jo-app/src/components/DashBoard/DashBoardCard/style.js
@@ -1,15 +1,24 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 100%;
-  max-width: 16.375rem;
+  display: flex;
+  width: 16.375rem;
   height: 9.625rem;
+  flex-direction: column;
   border-radius: 0.5rem;
   background-color: white;
   padding: 2rem 1.5rem;
-
   &:first-child {
     background-color: black;
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+    height: 6.625rem;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 1rem;
   }
 `;
 
@@ -19,6 +28,9 @@ export const Classification = styled.p`
   line-height: 1.188rem;
   color: ${(props) => (props.isFirstCard ? "white" : "var(--color-text)")};
   margin-bottom: 1.5rem;
+  @media (max-width: 768px) {
+    margin-bottom: 0rem;
+  }
 `;
 
 export const Data = styled.h2`
@@ -26,6 +38,10 @@ export const Data = styled.h2`
   font-size: 2.5rem;
   line-height: 1.6888rem;
   color: ${(props) => (props.isFirstCard ? "white" : "var(--color-text)")};
+  @media (max-width: 768px) {
+    font-size: 2.25rem;
+    line-height: 2.625rem;
+  }
 `;
 
 export const Unit = styled.label`
--- frontend/don-jo-app/src/components/DashBoard/DashBoardEarning/style.js
+++ frontend/don-jo-app/src/components/DashBoard/DashBoardEarning/style.js
@@ -2,15 +2,25 @@ import styled from "styled-components";
 
 export const EarningWrapper = styled.div`
   width: 100%;
-  max-height: 15.813rem;
-  height: 15.813rem;
   background-color: var(--color-modal);
   border-radius: 1.25rem;
   padding: 2rem 1.5rem;
   margin-bottom: 2.5rem;
+
+  @media (min-width: 769px) {
+    max-height: 15.813rem;
+    height: 15.813rem;
+  }
+  @media (max-width: 768px) {
+    background-color: transparent;
+    margin-bottom: 0.5rem;
+  }
 `;
 
 export const CardWrapper = styled.div`
   display: flex;
   justify-content: space-between;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
