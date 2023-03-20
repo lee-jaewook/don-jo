aaa2fd3f7701c34d9240936915c985b6465831a0
Hyunsu <83157338+hyunssu@users.noreply.github.com>
2023-03-20 13:49:57 +0900

    :construction: in progress : 대시보드 공통 컴포넌트 작업중
    대시보드 공통 컴포넌트 작업중 - 선택바

--- frontend/don-jo-app/src/components/DashBoard/CustomSelect/style.js
+++ frontend/don-jo-app/src/components/DashBoard/CustomSelect/style.js
@@ -1,56 +1,67 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const SelectBox = styled.div`
   position: relative;
-  /* display: flex;
-  align-items: center; */
   align-self: center;
   text-align: center;
   cursor: pointer;
   width: 100%;
-  padding: 8px;
   height: 2.75rem;
   border-radius: 0.5rem;
   background-color: white;
-  /* box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); */
-  &::before {
-    content: "⌵";
-    position: absolute;
-    top: 0.5rem;
-    right: 1rem;
-    color: #49c181;
-    font-size: 20px;
-  }
+  border: ${(props) =>
+    props.isDropdown ? "1px solid rgba(1, 0, 0, 1)" : "none"};
+
+  ${(props) =>
+    props.isBefore &&
+    css`
+      &::before {
+        content: "⌵";
+        position: absolute;
+        top: 0.5rem;
+        right: 1rem;
+        color: #49c181;
+        font-size: 1.25rem;
+      }
+    `}
 `;
 
 export const Label = styled.label`
-  font-size: 14px;
-  margin-left: 4px;
+  display: flex;
+  align-items: center;
+  padding-left: 12px;
+  font-size: 1rem;
   text-align: center;
+  height: 2.75rem;
+  font-style: ${(props) => props.fontStyle};
 `;
-
-export const Icon = styled.div``;
 
 export const SelectOptions = styled.ul`
   position: absolute;
   list-style: none;
-  top: 2.75rem;
+  top: 3rem;
   left: 0;
   width: 100%;
   overflow: hidden;
-  height: 90px;
   max-height: ${(props) => (props.show ? "none" : "0")};
   padding: 0;
   border-radius: 8px;
-  background-color: #222222;
-  color: #fefefe;
+  background-color: transparent;
+  color: var(--color-text);
 `;
 
 export const Option = styled.li`
-  font-size: 14px;
+  display: flex;
+  padding-left: 12px;
+  align-items: center;
+  text-align: center;
+  font-size: 16px;
+  height: 2.75rem;
+  border-radius: 8px;
   padding: 6px 8px;
   transition: background-color 0.2s ease-in;
   &:hover {
-    background-color: #595959;
+    background-color: var(--color-placeholder);
+    font-weight: 600;
   }
 `;
--- frontend/don-jo-app/src/pages/Intro/index.js
+++ frontend/don-jo-app/src/pages/Intro/index.js
@@ -1,25 +1,7 @@
-import React, { useState, useEffect } from "react";
-import * as S from "./style";
-import LogInModal from "../../components/LogIn";
-import GeneratorModal from "../../components/DashBoard/GeneratorModal";
+import React from "react";
 
 const Intro = () => {
-  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
-  const [isSignUpMOodalOpen, setIsSignUpModalOpen] = useState(false);
-  const [isGeneratorOpen, setIsGeneratorOpen] = useState(false);
-  return (
-    <div>
-      <button onClick={() => setIsLoginModalOpen((prev) => !prev)}>
-        눌러주세요
-      </button>
-      {isLoginModalOpen && (
-        <LogInModal isModelOpen={setIsLoginModalOpen}></LogInModal>
-      )}
-      <div></div>
-      <button onClick={setIsGeneratorOpen}>저는 슬퍼요</button>
-      {isGeneratorOpen && <GeneratorModal isModalOpen={setIsGeneratorOpen} />}
-    </div>
-  );
+  return <div>Intro</div>;
 };
 
 export default Intro;
