7963c85a183816b21c7c6167857c398576c221e5
Hyunsu <83157338+hyunssu@users.noreply.github.com>
2023-03-20 15:45:10 +0900

    :zap: update : 플러그인 생성 버튼과 모달 연결
    .

--- frontend/don-jo-app/src/components/DashBoard/DashBoardButtons/GeneratorItem/index.jsx
+++ frontend/don-jo-app/src/components/DashBoard/DashBoardButtons/GeneratorItem/index.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./style";
 import PropTypes from "prop-types";
-const GeneratorItem = ({ title, description }) => {
+import PluginGenerator from "../../DashBoardGeneratorModal";
+/**
+ * GeneratorItem 컴포넌트
+ * @param {Object} props - 컴포넌트에 전달되는 props
+ * @param {string} title - PlugIn 유형
+ * @param {string} description - PlugIn에 대한 설명
+ * @param {boolean} isItemsRequired - searchItems 컴포넌트 필수 여부
+ * @returns
+ */
+
+const GeneratorItem = ({ title, description, isItemsRequired }) => {
+  const [isPlugInGenModalOpen, setIsPlugInGenModalOpen] = useState(false);
+
+  const handlePlugInGenModalChange = () => {
+    setIsPlugInGenModalOpen((prev) => !prev);
+  };
+
   return (
-    <S.ItemWrapper>
-      <S.ItemImg />
-      <S.ItemInfo>
-        <S.Title>{title}</S.Title>
-        <S.Description>{description}</S.Description>
-      </S.ItemInfo>
-      <S.generateButton>Generate</S.generateButton>
-    </S.ItemWrapper>
+    <>
+      <S.ItemWrapper>
+        <S.ItemImg />
+        <S.ItemInfo>
+          <S.Title>{title}</S.Title>
+          <S.Description>{description}</S.Description>
+        </S.ItemInfo>
+        <S.generateButton onClick={handlePlugInGenModalChange}>
+          Generate
+        </S.generateButton>
+      </S.ItemWrapper>
+      {isPlugInGenModalOpen && (
+        <PluginGenerator isModalOpen={handlePlugInGenModalChange} />
+      )}
+    </>
   );
 };
 
