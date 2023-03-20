aca4d2837f52d6b5ae6c835063150b683076b96d
안태현 <dksxogus1012@gmail.com>
2023-03-20 22:16:39 +0900

    :sparkles: feat: Header 라우터 이동 설정

--- frontend/don-jo-app/src/components/Common/Header/index.jsx
+++ frontend/don-jo-app/src/components/Common/Header/index.jsx
@@ -1,21 +1,43 @@
 import * as S from "./style";
 import ProfileImg from "../ProfileImg";
 import SelectBox from "./SelectBox";
+import { Link, useLocation } from "react-router-dom";
+import homeIcon from "../../../assets/img/common/home.png";
+import { useEffect, useState } from "react";
 
 const Header = () => {
-  //임시 프로필 이미지 주소
-  const src =
-    "https://file.mk.co.kr/meet/neds/2022/10/image_readtop_2022_867768_16645834155184067.jpeg";
+  //로그인 유저 더미데이터
+  const loginUser = {
+    profileImgPath:
+      "https://img.insight.co.kr/static/2023/01/06/700/img_20230106141320_ai905341.webp",
+    memberAddress: "taehyun",
+  };
+
+  const location = useLocation();
+  const [profileImgSrc, setProfileImgSrc] = useState("");
+  const [profileLinkTo, setProfileLinkTo] = useState("");
+
+  useEffect(() => {
+    if (location.pathname === "/dashboard") {
+      setProfileImgSrc(homeIcon);
+      setProfileLinkTo(`/${loginUser.memberAddress}`);
+    } else {
+      setProfileImgSrc(loginUser.profileImgPath);
+      setProfileLinkTo("/dashboard");
+    }
+  }, [location.pathname]);
 
   return (
     <S.HeaderContainer>
       <S.Header>
-        <S.Logo />
+        <Link to="/">
+          <S.Logo />
+        </Link>
         <S.GuideSelect>
           <SelectBox />
         </S.GuideSelect>
         <S.ProfileImgContainer>
-          <ProfileImg width={2.5} src={src} />
+          <ProfileImg width={2.5} src={profileImgSrc} to={profileLinkTo} />
         </S.ProfileImgContainer>
       </S.Header>
     </S.HeaderContainer>
