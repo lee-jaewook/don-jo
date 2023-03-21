fb26719698830cbb110100b20a2e0d51cede9cc1
안태현 <dksxogus1012@gmail.com>
2023-03-21 17:38:02 +0900

    :bug: fix: 풀스크린 모달 외부 스크롤 수정

--- frontend/don-jo-app/src/components/Common/Modal/FullScreenModal/index.jsx
+++ frontend/don-jo-app/src/components/Common/Modal/FullScreenModal/index.jsx
@@ -6,12 +6,12 @@ import { useEffect } from "react";
 const FullScreenModal = ({ handleSetShowModal, children }) => {
   //모달 열릴때 외부 요소 스크롤 막기
   useEffect(() => {
-    document.body.style.overflowY = "hidden";
+    document.body.style.overflow = "hidden";
   }, []);
 
   //모달 닫힐 때 외부 요소 스크롤 허용
   const closeModal = () => {
-    document.body.style.overflowY = "auto";
+    document.body.style.overflow = "auto";
     handleSetShowModal(false);
   };
 
