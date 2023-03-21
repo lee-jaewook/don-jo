a69ae802067f0dbad630252490760042568c417e
Hyunsu <83157338+hyunssu@users.noreply.github.com>
2023-03-22 08:51:29 +0900

    :memo: add : add contract.
    .

--- frontend/don-jo-app/src/components/DashBoard/DashBoardGeneratorModal/index.jsx
+++ frontend/don-jo-app/src/components/DashBoard/DashBoardGeneratorModal/index.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
+import * as S from "./style";
+import PropTypes from "prop-types";
+
+import { colorSet } from "../../../data/dashboard";
+
 import BasicInput from "../../Common/BasicInput";
-// import SelectBox from "../../Common/SelectBox";/
 import BasicModal from "../../Common/Modal/BasicModal";
 import BasicTitle from "../../Common/BasicTitle";
 import CustomSelect from "../DashBoardCustomSelect";
-import PropTypes from "prop-types";
-import * as S from "./style";
-import { colorSet } from "../../../data/dashboard";
+
+import { useSelector, useDispatch } from "react-redux";
+import ApplicationHandler from "../../../contracts/ApplicationHandler.json";
 
 /**
  * 플러그인 생성기 컴포넌트
@@ -24,6 +28,31 @@ const GeneratorModal = ({
 }) => {
   const [title, setTitle] = useState("");
   const [colorIndex, setColorIndex] = useState("#F02C7E"); // 사용자의 현재 테마 색상 설정
+
+  // test 용
+
+  const web3 = useSelector((state) => state.web3.web3);
+  console.log(web3.eth);
+  const myContract = new web3.eth.Contract(
+    ApplicationHandler.abi,
+    "0xdBEeDa3fB7d11CC14638A8994b16db785Db0B7d8"
+  );
+
+  // 지갑주소
+  myContract.methods
+    .callBasicDonation("0x6c3ea1dD30BEb9B449272d393693A47727a5dF12")
+    .send({
+      from: "0x0964fB71FB405f75fd95bAe71825Cc2d697E4eE9",
+      to: "0x6c3ea1dD30BEb9B449272d393693A47727a5dF12",
+      value: 1000000,
+    })
+    .on("receipt", (receipt) => {
+      console.log("트랜잭션이 완료되었습니다.", receipt);
+    })
+    .on("error", (error) => {
+      console.error("트랜잭션 실행 중 오류가 발생했습니다.", error);
+    });
+  /////////////////////////////////////////////////////////////////////
 
   const handleTitleChange = (e) => {
     setTitle(e.target.value);
