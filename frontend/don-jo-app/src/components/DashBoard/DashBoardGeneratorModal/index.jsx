b4050fd3b625aebe93ffba258c6a13114b7f4503
Hyunsu <83157338+hyunssu@users.noreply.github.com>
2023-03-22 10:27:46 +0900

    :zap: update : PlugIn Modal UI 완성.
    .

--- frontend/don-jo-app/src/components/DashBoard/DashBoardGeneratorModal/index.jsx
+++ frontend/don-jo-app/src/components/DashBoard/DashBoardGeneratorModal/index.jsx
@@ -4,13 +4,17 @@ import PropTypes from "prop-types";
 
 import { colorSet } from "../../../data/dashboard";
 
+import BasicButton from "../../Common/BasicButton";
 import BasicInput from "../../Common/BasicInput";
 import BasicModal from "../../Common/Modal/BasicModal";
 import BasicTitle from "../../Common/BasicTitle";
 import CustomSelect from "../DashBoardCustomSelect";
+import EmojiPicker from "emoji-picker-react";
+import { useInput } from "../../../hooks/useInput";
+import { FiChevronDown } from "react-icons/fi";
 
-import { useSelector, useDispatch } from "react-redux";
-import ApplicationHandler from "../../../contracts/ApplicationHandler.json";
+// import { useSelector, useDispatch } from "react-redux";
+// import ApplicationHandler from "../../../contracts/ApplicationHandler.json";
 
 /**
  * 플러그인 생성기 컴포넌트
@@ -24,39 +28,28 @@ import ApplicationHandler from "../../../contracts/ApplicationHandler.json";
 const GeneratorModal = ({
   isSearchDefault,
   isModalOpen,
-  isItemsRequired = false,
+  isItemsRequired = true,
 }) => {
   const [title, setTitle] = useState("");
   const [colorIndex, setColorIndex] = useState("#F02C7E"); // 사용자의 현재 테마 색상 설정
+  const [selectedEmoji, setSelectedEmoji] = useState("💕"); // user별 default emoji 설정
+  const [emojiName, onChangeEmojiName] = useInput("Heart"); // user별 default emoji 이름 설정
 
-  // test 용
+  const [isShowEmojiPicker, setShowEmojiPicker] = useState(false);
+  const handleSetShowEmojiPicker = () => setShowEmojiPicker((prev) => !prev);
 
-  const web3 = useSelector((state) => state.web3.web3);
-  console.log(web3.eth);
-  const myContract = new web3.eth.Contract(
-    ApplicationHandler.abi,
-    "0xdBEeDa3fB7d11CC14638A8994b16db785Db0B7d8"
-  );
-
-  // 지갑주소
-  myContract.methods
-    .callBasicDonation("0x6c3ea1dD30BEb9B449272d393693A47727a5dF12")
-    .send({
-      from: "0x0964fB71FB405f75fd95bAe71825Cc2d697E4eE9",
-      to: "0x6c3ea1dD30BEb9B449272d393693A47727a5dF12",
-      value: 1000000,
-    })
-    .on("receipt", (receipt) => {
-      console.log("트랜잭션이 완료되었습니다.", receipt);
-    })
-    .on("error", (error) => {
-      console.error("트랜잭션 실행 중 오류가 발생했습니다.", error);
-    });
-  /////////////////////////////////////////////////////////////////////
+  const handleOnClickEmoji = (item) => {
+    console.log(item);
+    setSelectedEmoji(item.emoji);
+    onChangeEmojiName(item.names[0]);
+    setShowEmojiPicker(false);
+  };
 
   const handleTitleChange = (e) => {
     setTitle(e.target.value);
   };
+
+  const handleGeneratePlugIn = () => {};
 
   return (
     <div>
@@ -66,21 +59,28 @@ const GeneratorModal = ({
         </S.PreViewWrap>
 
         <S.ContentWrap>
-          {/* Text Component */}
           <BasicTitle text="Text" />
-          <S.GridBox>
-            {/* <SelectBox /> */}
-            <BasicInput
-              type="text"
-              value={title}
-              handleOnChangeValue={handleTitleChange}
-              placeholder="Plz give me money"
-            />
-          </S.GridBox>
+          <S.EmojiSettingWrapper>
+            <S.GridBox>
+              <S.EmojiButton onClick={handleSetShowEmojiPicker}>
+                {selectedEmoji}
+                <FiChevronDown size="16px" />
+              </S.EmojiButton>
+              <BasicInput
+                type="text"
+                value={title}
+                handleOnChangeValue={handleTitleChange}
+              />
+            </S.GridBox>
+            {isShowEmojiPicker && (
+              <S.EmojiPickerModal>
+                <EmojiPicker onEmojiClick={handleOnClickEmoji} />
+              </S.EmojiPickerModal>
+            )}
+          </S.EmojiSettingWrapper>
         </S.ContentWrap>
 
         <S.ContentWrap>
-          {/* Color Component */}
           <BasicTitle text="Color" />
           <S.ColorPalette>
             {colorSet &&
@@ -99,7 +99,6 @@ const GeneratorModal = ({
         </S.ContentWrap>
 
         <S.ContentWrap>
-          {/* Font Component */}
           <BasicTitle text="Font" />
           <CustomSelect isBefore={true} />
         </S.ContentWrap>
@@ -113,6 +112,17 @@ const GeneratorModal = ({
             </>
           )}
         </S.ContentWrap>
+
+        <S.ButtonWrap>
+          <S.ButtonContent>
+            <BasicButton
+              text="Generate"
+              handleOnClickButton={handleGeneratePlugIn}
+              isBackground="var(--color-primary)"
+              isDisabled={false}
+            />
+          </S.ButtonContent>
+        </S.ButtonWrap>
       </BasicModal>
     </div>
   );
@@ -125,3 +135,28 @@ GeneratorModal.propTypes = {
   isModalOpen: PropTypes.func.isRequired,
   isItemsRequired: PropTypes.bool,
 };
+
+// test 용
+
+// const web3 = useSelector((state) => state.web3.web3);
+// console.log(web3.eth);
+// const myContract = new web3.eth.Contract(
+//   ApplicationHandler.abi,
+//   "0xdBEeDa3fB7d11CC14638A8994b16db785Db0B7d8"
+// );
+
+// 지갑주소
+// myContract.methods
+//   .callBasicDonation("0x6c3ea1dD30BEb9B449272d393693A47727a5dF12")
+//   .send({
+//     from: "0x0964fB71FB405f75fd95bAe71825Cc2d697E4eE9",
+//     to: "0x6c3ea1dD30BEb9B449272d393693A47727a5dF12",
+//     value: 1000000,
+//   })
+//   .on("receipt", (receipt) => {
+//     console.log("트랜잭션이 완료되었습니다.", receipt);
+//   })
+//   .on("error", (error) => {
+//     console.error("트랜잭션 실행 중 오류가 발생했습니다.", error);
+//   });
+/////////////////////////////////////////////////////////////////////
--- frontend/don-jo-app/src/components/DashBoard/DashBoardGeneratorModal/style.js
+++ frontend/don-jo-app/src/components/DashBoard/DashBoardGeneratorModal/style.js
@@ -11,7 +11,6 @@ export const PreViewWrap = styled.div`
 
 export const ContentWrap = styled.div`
   width: 100%;
-  display: flex;
   margin-bottom: 2rem;
 `;
 
@@ -27,7 +26,9 @@ export const PreView = styled.div`
 
 export const GridBox = styled.div`
   display: grid;
-  grid-template-columns: 20% 80%;
+  grid-template-columns: 1fr 2fr;
+  grid-gap: 0.75rem;
+  height: 2.75rem;
 `;
 
 export const ColorPalette = styled.div`
@@ -54,4 +55,45 @@ export const Color = styled.input`
       color: white;
     }
   }
+`;
+
+export const EmojiSettingWrapper = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+export const EmojiButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+  height: 2.75rem;
+  font-size: 1.5rem;
+  line-height: 1.75rem;
+  border: none;
+  background-color: white;
+  border-radius: 0.5rem;
+  margin-right: 0.75rem;
+`;
+
+export const EmojiPickerModal = styled.div`
+  position: absolute !important;
+  top: 3rem;
+  left: 0rem;
+  z-index: 10;
+`;
+
+export const ButtonWrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 2.75rem;
+  margin-top: 0.5rem;
+`;
+
+export const ButtonContent = styled.div`
+  width: 100%;
+  height: 100%;
+  max-width: 17.5rem;
 `;
