b577b8b2846a34e0df9de94c524ef4791c92f562
Hyunsu <83157338+hyunssu@users.noreply.github.com>
2023-03-22 13:33:07 +0900

    :zap: update : AddItemModal 연결 코드 추가
    AddItemModal 연결 코드 추가

--- frontend/don-jo-app/src/components/DashBoard/DashBoardItems/ItemsSettings/index.jsx
+++ frontend/don-jo-app/src/components/DashBoard/DashBoardItems/ItemsSettings/index.jsx
@@ -1,44 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BasicTitle from "../../../Common/BasicTitle";
 import * as S from "./style";
 import { FiPlus } from "react-icons/fi";
 import ListItem from "./ListItem";
 import { itemList } from "../../../../data/dashboard";
 import ShowMoreButton from "../../../Common/ShowMoreButton";
+import AddItemModal from "../AddItemModal";
 const ItemsSettings = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const handleGetMyItemList = () => {
     console.log("handleGetMyItemList");
   };
 
+  const handleModalOpen = () => {
+    setIsModalOpen((prev) => !prev);
+  };
+
   return (
-    <S.SettingWrapper>
-      <S.AddButton>
-        <S.AddIcon>
-          <FiPlus size="32px" color="white" />
-        </S.AddIcon>
-      </S.AddButton>
-      <BasicTitle text="Items List" />
-      <div>
-        {itemList && itemList.length > 0 ? (
-          itemList.map((item, index) => (
-            <ListItem
-              key={index + item.uid}
-              uid={item.uid}
-              imgPath={item.imgPath}
-              supportCount={item.supportCount}
-              title={item.title}
-              collectedAmount={item.collectedAmount}
-              totalAmount={item.totalAmount}
-            />
-          ))
-        ) : (
-          <label>There are no items registered.</label>
-        )}
-      </div>
-      {itemList.length > 4 && (
-        <ShowMoreButton handleOnClickButton={handleGetMyItemList} />
+    <>
+      {!isModalOpen ? (
+        <S.SettingWrapper>
+          <S.AddButton>
+            <S.AddIcon onClick={handleModalOpen}>
+              <FiPlus size="32px" color="white" />
+            </S.AddIcon>
+          </S.AddButton>
+          <BasicTitle text="Items List" />
+          <div>
+            {itemList && itemList.length > 0 ? (
+              itemList.map((item, index) => (
+                <ListItem
+                  key={index + item.uid}
+                  uid={item.uid}
+                  imgPath={item.imgPath}
+                  supportCount={item.supportCount}
+                  title={item.title}
+                  collectedAmount={item.collectedAmount}
+                  totalAmount={item.totalAmount}
+                />
+              ))
+            ) : (
+              <label>There are no items registered.</label>
+            )}
+          </div>
+          {itemList.length > 4 && (
+            <ShowMoreButton handleOnClickButton={handleGetMyItemList} />
+          )}
+        </S.SettingWrapper>
+      ) : (
+        <AddItemModal handleSetShowModal={handleModalOpen} />
       )}
-    </S.SettingWrapper>
+    </>
   );
 };
 
