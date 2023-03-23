38e02c7ad27b298345d55b8ae86b04bc2a76ef39
송현주 <shyunju7@naver.com>
2023-03-23 13:27:06 +0900

    ✨ feat: dashboard account 이미지 업로드 API 연결 및 처리

--- frontend/don-jo-app/src/components/DashBoard/DashBoardAccount/index.jsx
+++ frontend/don-jo-app/src/components/DashBoard/DashBoardAccount/index.jsx
@@ -5,14 +5,24 @@ import BasicButton from "../../Common/BasicButton";
 import BasicInput from "../../Common/BasicInput";
 import BasicTitle from "../../Common/BasicTitle";
 import { colorSet } from "../../../data/dashboard";
+import { fileApi } from "../../../api/file";
 
 const DashBoardAccount = () => {
+  const PROFILE_TYPE = "img/profile";
+  const BACKGROUND_TYPE = "img/background";
+
   // 업로드 파일 미리보기
   const backgroundImgRef = useRef();
   const profileRef = useRef();
 
-  const [backgroundImgFile, setBackgroundImgFile] = useState(""); // 사용자의 기본 배경 설정
-  const [profileImgFile, setProfileImgFile] = useState(""); // 사용자의 기본 프로필 설정
+  const [backgroundImgFile, setBackgroundImgFile] = useState({
+    previewImgUrl: "",
+    file: {},
+  }); // 사용자의 기본 배경 설정
+  const [profileImgFile, setProfileImgFile] = useState({
+    previewImgUrl: "",
+    file: {},
+  }); // 사용자의 기본 프로필 설정
   const [account, setAccount] = useState({
     nickname: "HyunJu",
     pageName: "songo427",
@@ -26,46 +36,68 @@ const DashBoardAccount = () => {
 
   const handleOnChangeInput = (e) => {
     const { id, value } = e.target;
-
     setAccount({
       ...account,
       [id]: value,
     });
   };
 
-  const handleSaveImgFile = (e) => {
+  // 이미지 미리보기를 위한 함수
+  const handleMakePreviewImg = (e) => {
     const {
-      target: { id },
+      target: { id, files },
     } = e;
-    if (id === "bg-file" && backgroundImgRef.current !== null) {
-      const file = backgroundImgRef.current.files[0];
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onloadend = () => {
-        setBackgroundImgFile(reader.result);
-      };
-      return;
-    }
 
-    if (id === "profile-file" && profileRef.current !== null) {
-      const file = profileRef.current.files[0];
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onloadend = () => {
-        setProfileImgFile(reader.result);
-      };
-      return;
-    }
+    const reader = new FileReader();
+    reader.readAsDataURL(files[0]);
+    reader.onloadend = () => {
+      if (id === "bg-file") {
+        setBackgroundImgFile({ previewImgUrl: reader.result, file: files[0] });
+        return;
+      } else if (id === "profile-file") {
+        setProfileImgFile({ previewImgUrl: reader.result, file: files[0] });
+        return;
+      }
+    };
   };
 
-  const handleSaveAccount = () => {
-    console.log("handleSaveAccount()...");
+  const handleSaveAccount = async () => {
+    // 프로필 이미지 업로드
+    const profileImgPath = await handleUploadFile(
+      profileImgFile.file,
+      PROFILE_TYPE
+    );
+    // 배경 이미지 업로드
+    const backgroundImgPath = await handleUploadFile(
+      backgroundImgFile.file,
+      BACKGROUND_TYPE
+    );
+
+    const myAccount = {
+      ...account,
+      profileImgPath,
+      backgroundImgPath,
+    };
+    // account 객체에 모든 내용이 있는지 확인하고 수정 API 호출
+    console.log("[API 호출 시점] account : ", myAccount);
+  };
+
+  const handleUploadFile = async (file, type) => {
+    const formData = new FormData();
+    formData.append("multipartFile", file);
+
+    try {
+      const { data } = await fileApi.uploadFile(formData, type);
+      return data;
+    } catch (error) {
+      console.log("error: ", error);
+    }
   };
 
   return (
     <S.AccountWrapper>
       <BasicTitle text="Background Photo" />
-      <S.BackgroundImg url={backgroundImgFile || ""}>
+      <S.BackgroundImg url={backgroundImgFile.previewImgUrl || ""}>
         <S.EditIconWrapper>
           <label htmlFor="bg-file">
             <FiEdit className="edit-icon" size="24px" color="white" />
@@ -75,12 +107,12 @@ const DashBoardAccount = () => {
             id="bg-file"
             type="file"
             accept="image/*"
-            onChange={handleSaveImgFile}
+            onChange={handleMakePreviewImg}
           />
         </S.EditIconWrapper>
       </S.BackgroundImg>
       <BasicTitle text="Profile Photo" />
-      <S.UserProfileImg url={profileImgFile || ""}>
+      <S.UserProfileImg url={profileImgFile.previewImgUrl || ""}>
         <S.EditIconWrapper>
           <label htmlFor="profile-file">
             <FiEdit className="edit-icon" size="24px" color="white" />
@@ -90,7 +122,7 @@ const DashBoardAccount = () => {
             id="profile-file"
             type="file"
             accept="image/*"
-            onChange={handleSaveImgFile}
+            onChange={handleMakePreviewImg}
           />
         </S.EditIconWrapper>
       </S.UserProfileImg>
