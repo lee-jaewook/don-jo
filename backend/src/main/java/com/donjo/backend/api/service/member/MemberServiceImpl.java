e8b4e121bc1c2c9ca838483ec3e24e1425399a55
Taesun0727 <77261999+Taesun0727@users.noreply.github.com>
2023-03-23 13:30:56 +0900

    :sparkles: feat: 멤버 정보 수정 기능

--- backend/src/main/java/com/donjo/backend/api/service/member/MemberServiceImpl.java
+++ backend/src/main/java/com/donjo/backend/api/service/member/MemberServiceImpl.java
@@ -14,15 +14,14 @@ import com.donjo.backend.config.jwt.TokenProvider;
 import com.donjo.backend.db.entity.Authority;
 import com.donjo.backend.db.entity.DonationSetting;
 import com.donjo.backend.db.entity.Member;
+import com.donjo.backend.db.entity.Social;
 import com.donjo.backend.db.repository.MemberRepository;
-import com.donjo.backend.db.repository.SupportRepository;
 import com.donjo.backend.exception.BadRequestException;
 import com.donjo.backend.exception.DuplicateDataException;
 import com.donjo.backend.exception.DuplicateMemberException;
 
 import com.donjo.backend.exception.NoContentException;
 
-import com.donjo.backend.solidity.support.SupportSolidity;
 import java.util.*;
 
 import com.donjo.backend.exception.UnAuthorizationException;
@@ -45,8 +44,6 @@ public class MemberServiceImpl implements MemberService {
   private final TokenProvider tokenProvider;
   private final String PAGE_NAME = "pageName";
   private final WishlistSolidity wishlistSolidity;
-  private final SupportSolidity supportSolidity;
-  private final SupportRepository supportRepository;
 
   @Override
   public Optional<Member> findMember(String memberAddress) {
@@ -70,6 +67,11 @@ public class MemberServiceImpl implements MemberService {
 
     Authority userAuthority = Authority.user();
 
+    List<Social> socialList = new ArrayList<>();
+    socialList.add(Social.builder().socialLink("").build());
+    socialList.add(Social.builder().socialLink("").build());
+    socialList.add(Social.builder().socialLink("").build());
+
     Member member = Member.builder()
         .address(signUpMemberCond.getAddress())
         .nickname(signUpMemberCond.getNickname())
@@ -77,6 +79,7 @@ public class MemberServiceImpl implements MemberService {
         .password(passwordEncoder.encode(signUpMemberCond.getPassword()))
         .profileImagePath(signUpMemberCond.getProfileImgPath())
         .authorities(Set.of(userAuthority))
+        .social(socialList)
         .build();
 
     DonationSetting donationSetting = DonationSetting.builder()
@@ -179,9 +182,18 @@ public class MemberServiceImpl implements MemberService {
   @Transactional
   public void modifyMemberInfo(String memberAdress, ModifyMemberCond modifyMemberCond) {
     Member member = memberRepository.findByAddress(memberAdress);
+
+    // 유저 체크
     if (member == null) {
       new NotFoundException("유저 정보가 없습니다.");
     }
+
+    // 중복 페이지 검사
+    Member duplicatePageMember = memberRepository.findByPageName(modifyMemberCond.getPageName());
+    if (duplicatePageMember != null && !member.getAddress().equals(duplicatePageMember.getAddress()) && modifyMemberCond.getPageName().equals(duplicatePageMember.getPageName())) {
+      throw new DuplicateMemberException("다른 유저의 페이지 네임과 중복");
+    }
+
     modifyMemberCond.updateMember(member);
 
   }
