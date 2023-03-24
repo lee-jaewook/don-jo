9d5caaf978510ce159f322336b31d45017ae9d14
이재욱 <jaewook_lee@outlook.com>
2023-03-24 14:43:14 +0900

    update

--- frontend-guide/my-website/docusaurus.config.js
+++ frontend-guide/my-website/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: "https://j8a209.p.ssafy.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/guides/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
