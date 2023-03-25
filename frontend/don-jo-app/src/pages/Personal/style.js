81d0236598972811d0f664e30ec3948740ad350f
안태현 <dksxogus1012@gmail.com>
2023-03-25 14:27:01 +0900

    :lipstick: design: 개인페이지와 푸터 사이 여백 생성

--- frontend/don-jo-app/src/pages/Personal/style.js
+++ frontend/don-jo-app/src/pages/Personal/style.js
@@ -6,6 +6,7 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   position: relative;
+  margin-bottom: 9.375rem;
 `;
 
 export const BackgroundImg = styled.div`
