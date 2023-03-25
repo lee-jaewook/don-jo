d4b337d9e5f951ac367df111a2a0a34efd542dc6
안태현 <dksxogus1012@gmail.com>
2023-03-25 14:26:22 +0900

    :sparkles: feat: 개인페이지 Home api 호출

--- frontend/don-jo-app/src/pages/Personal/index.jsx
+++ frontend/don-jo-app/src/pages/Personal/index.jsx
@@ -1,60 +1,19 @@
 import * as S from "./style";
 import { FiEdit } from "react-icons/fi";
 import ExternalLink from "../../components/Personal/ExternalLink";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PersonalContent from "../../components/Personal/PersonalContent";
 import FullScreenModal from "../../components/Common/Modal/FullScreenModal";
 import IntroductionEdit from "../../components/Personal/IntroductionEdit";
 import MDEditor from "@uiw/react-md-editor";
 import { Desktop } from "../../components/Common/Template";
+import { memberApi } from "../../api/member";
 
 const Personal = () => {
   //로그인 유저 더미 데이터
   const loginUser = {
-    memberAddress: "memberaddress",
     nickname: "taehyun",
-  };
-
-  //해당 페이지 사람 더미 데이터
-  const pageOwner = {
-    memberAddress: "memberaddress",
-    profileImgPath:
-      "https://img.insight.co.kr/static/2023/01/06/700/img_20230106141320_ai905341.webp",
-    backgroundImgPath:
-      "https://cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/Q5WX26BXPG3CB5COPKO6AU2P54.png",
-    nickname: "Robert Downey Jr.",
-    introduction: `# Its me
-
-Hi, My name is Robert Downy Jr.
-
-> This is my personal page for sponsorship.
-
-Please take a look at my work and send me a message of support. 
-
-—————
-- item 1
-- item 2
-- item 3  
-—————
-
-\`\`\`java
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
-\`\`\`
-
-
-**Thank you.**
-
-![image](https://i.ytimg.com/vi/FZhIEzWjb5w/maxresdefault.jpg)`,
-    numSupporters: 16000,
-    socialList: [
-      "https://www.youtube.com/@SamsungKorea",
-      "https://velog.io/@taebong1012",
-      "https://github.com/taebong1012",
-    ],
+    pageName: "taebong",
   };
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -63,55 +22,87 @@ public class HelloWorld {
   const [isProfileHover, setIsProfileHover] = useState(false);
   const [isShowIntroductionEdit, setIsShowIntroductionEdit] = useState(false);
 
+  const curPageName = "taebong";
+  const [donationSettingData, setDonationSettingData] = useState({
+    donationEmoji: "",
+    donationName: "",
+    pricePerDonation: 0,
+    thankMsg: "",
+  });
+  const [memberInfoItemData, setMemberInfoItemData] = useState({
+    backgroundImgPath: null,
+    introduction: null,
+    memberAddress: "",
+    nickname: "",
+    numSupporters: 0,
+    profileImgPath: "",
+    socialList: ["", "", ""],
+    themeColor: 1,
+  });
+  const [wishListData, setWishListData] = useState([]);
+
+  const getPageInfo = async () => {
+    try {
+      const { data } = await memberApi.getPageInfo(curPageName);
+      setMemberInfoItemData(data.memberInfoItem);
+      setDonationSettingData(data.donationSetting);
+      setWishListData(data.wishList);
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  };
+
+  useEffect(() => {
+    getPageInfo();
+  }, []);
+
   return (
     <S.Container>
       <S.BackgroundImg
-        src={pageOwner.backgroundImgPath}
+        src={memberInfoItemData.backgroundImgPath}
         onMouseOver={() => setIsBackgroundHover(true)}
         onMouseOut={() => setIsBackgroundHover(false)}
       >
-        {loginUser.memberAddress === pageOwner.memberAddress &&
-          isBackgroundHover && (
-            <S.BackgroundImgEdit>
-              <S.EditIcon>
-                <FiEdit color="white" size={20.35} />
-              </S.EditIcon>
-            </S.BackgroundImgEdit>
-          )}
+        {loginUser.pageName === curPageName && isBackgroundHover && (
+          <S.BackgroundImgEdit>
+            <S.EditIcon>
+              <FiEdit color="white" size={20.35} />
+            </S.EditIcon>
+          </S.BackgroundImgEdit>
+        )}
       </S.BackgroundImg>
       <S.ProfileImgContainer>
         <S.ProfileImg
-          src={pageOwner.profileImgPath}
+          src={memberInfoItemData.profileImgPath}
           onMouseOver={() => setIsProfileHover(true)}
           onMouseOut={() => setIsProfileHover(false)}
         >
-          {loginUser.memberAddress === pageOwner.memberAddress &&
-            isProfileHover && (
-              <S.ProfileImgEdit>
-                <S.EditIcon>
-                  <FiEdit color="white" size={20.35} />
-                </S.EditIcon>
-              </S.ProfileImgEdit>
-            )}
+          {loginUser.pageName === curPageName && isProfileHover && (
+            <S.ProfileImgEdit>
+              <S.EditIcon>
+                <FiEdit color="white" size={20.35} />
+              </S.EditIcon>
+            </S.ProfileImgEdit>
+          )}
         </S.ProfileImg>
       </S.ProfileImgContainer>
 
       <S.ContentsContainer>
         <S.UserInfo>
-          <S.Nickname>{pageOwner.nickname}</S.Nickname>
+          <S.Nickname>{memberInfoItemData.nickname}</S.Nickname>
           <S.SupporterContainer>
             <S.NumSupporter>
-              {pageOwner.numSupporters
+              {memberInfoItemData.numSupporters
                 .toString()
                 .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
             </S.NumSupporter>
             supporter
           </S.SupporterContainer>
-          <ExternalLink socialList={pageOwner.socialList} />
+          <ExternalLink socialList={memberInfoItemData.socialList} />
           <Desktop>
             <S.IntroductionContainer>
               {/* 로그인한 유저와 페이지 주인이 같다면 edit 버튼 표시 */}
-              {loginUser.memberAddress === pageOwner.memberAddress && (
+              {loginUser.pageName === curPageName && (
                 <S.IntroductionEdit
                   onClick={() => {
                     setIsShowIntroductionEdit(true);
@@ -122,13 +113,16 @@ public class HelloWorld {
               )}
               <S.Introduction>
                 <MDEditor.Markdown
-                  source={pageOwner.introduction}
+                  source={memberInfoItemData.introduction}
                 ></MDEditor.Markdown>
               </S.Introduction>
             </S.IntroductionContainer>
           </Desktop>
         </S.UserInfo>
-        <PersonalContent />
+        <PersonalContent
+          donationSettingData={donationSettingData}
+          wishListData={wishListData}
+        />
       </S.ContentsContainer>
 
       {isShowIntroductionEdit && (
