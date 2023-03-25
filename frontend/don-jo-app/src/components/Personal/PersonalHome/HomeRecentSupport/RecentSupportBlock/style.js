3ca1d21ff0e58b603b412e70e724fdcfb16b5a5c
안태현 <dksxogus1012@gmail.com>
2023-03-25 23:33:11 +0900

    :lipstick: design: 최근 후원 목록 hover 시 클릭 가능함 표시

--- frontend/don-jo-app/src/components/Personal/PersonalHome/HomeRecentSupport/RecentSupportBlock/style.js
+++ frontend/don-jo-app/src/components/Personal/PersonalHome/HomeRecentSupport/RecentSupportBlock/style.js
@@ -17,6 +17,12 @@ export const RepresentContainer = styled.div`
   display: flex;
   width: 100%;
   align-items: center;
+  padding: 0.375rem;
+  border-radius: 12px;
+
+  &:hover {
+    background-color: var(--color-background-secondary);
+  }
 `;
 
 export const ProfileImgContainer = styled.div`
