6f0ec0298c6d4c31a6e30701d77b98de6ecd544e
안태현 <dksxogus1012@gmail.com>
2023-03-28 00:17:36 +0900

    :sparkles: feat: Recent Support API 부착 준비 및 없을 경우 처리

--- frontend/don-jo-app/src/components/Personal/PersonalHome/HomeRecentSupport/RecentSupportBlock/index.jsx
+++ frontend/don-jo-app/src/components/Personal/PersonalHome/HomeRecentSupport/RecentSupportBlock/index.jsx
@@ -4,31 +4,7 @@ import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import ContractModal from "../../../../Common/Modal/ContractModal";
 
-//현재 로그인한 유저 더미 데이터
-const loginUser = {
-  memberAddress: "memberaddress",
-  nickname: "taehyun",
-};
-
-//해당 페이지 사람 더미 데이터
-const pageOwner = {
-  memberAddress: "memberaddress",
-  profileImgPath:
-    "https://img.insight.co.kr/static/2023/01/06/700/img_20230106141320_ai905341.webp",
-  backgroundImgPath:
-    "https://cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/Q5WX26BXPG3CB5COPKO6AU2P54.png",
-  nickname: "Robert Downey Jr.",
-  introduction:
-    "This is Example introduction. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. This is Example introduction. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
-  numSupporters: 16000,
-  socialList: [
-    "https://www.youtube.com/@SamsungKorea",
-    "https://velog.io/@taebong1012",
-    "https://github.com/taebong1012",
-  ],
-};
-
-const RecentSupportBlock = ({ supportContent }) => {
+const RecentSupportBlock = ({ supportContent, isOwner }) => {
   const [supportText, setSupportText] = useState("");
   const [emoji, setEmoji] = useState("");
   const [isShowReplyInput, setIsShowReplyInput] = useState(false);
@@ -63,96 +39,96 @@ const RecentSupportBlock = ({ supportContent }) => {
     setIsShowReplyInput(false);
   };
 
-  return (
-    <div>
-      {/* 누가 누구에게 어떠한 후원을 했는지 노출 */}
-      <S.Container>
-        <S.RepresentContainer
-          onClick={() => {
-            setIsShowContractModal(true);
-          }}
-        >
-          <S.ProfileImgContainer
-            onClick={(e) => {
-              e.stopPropagation();
-            }}
-          >
-            <ProfileImg
-              width={3}
-              src={supportContent.fromMember.profileImgPath}
-              to={`/${supportContent.fromMember.pageName}`}
-            />
-          </S.ProfileImgContainer>
-          <S.TitleWrapper>
-            <S.TitleContent>
-              <S.Nickname>{supportContent.fromMember.nickname}</S.Nickname>
-              &nbsp;
-              {supportText}
-              &nbsp;
-              <S.Nickname>{pageOwner.nickname}</S.Nickname>
-            </S.TitleContent>
-            {loginUser.memberAddress === pageOwner.memberAddress &&
-              Object.keys(supportContent.comments).length === 0 && (
-                <S.ReplyBtn
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    setIsShowReplyInput((prev) => !prev);
-                  }}
-                >
-                  {isShowReplyInput ? "Close" : "Reply"}
-                </S.ReplyBtn>
-              )}
-          </S.TitleWrapper>
-          <S.EmojiWrapper>
-            <label style={{ marginLeft: "0.5rem" }}>{emoji}</label>
-          </S.EmojiWrapper>
-        </S.RepresentContainer>
-        {loginUser.memberAddress === pageOwner.memberAddress &&
-          isShowReplyInput && (
-            <S.InputContainer>
-              <S.ReplyInput handleOnChangeValue={handleOnChangeInput} />
-              <S.RegistBtn onClick={doRegistComment}>Regist</S.RegistBtn>
-            </S.InputContainer>
-          )}
-      </S.Container>
-      {/* 서포트 메세지가 있을 경우 노출 */}
-      {supportContent.sendMsg !== "" && (
-        <S.CommentContainer>
-          <S.ProfileImgContainer>
-            <ProfileImg
-              width={3}
-              src={supportContent.fromMember.profileImgPath}
-              to={`/${supportContent.fromMember.pageName}`}
-            />
-          </S.ProfileImgContainer>
-          <div>
-            <S.Nickname>{supportContent.fromMember.nickname}</S.Nickname>
-            <S.Comment>{supportContent.sendMsg}</S.Comment>
-            <S.SupportMsgText>Support message</S.SupportMsgText>
-          </div>
-        </S.CommentContainer>
-      )}
-      {/* 해당 후원에 댓글이 있을 경우 노출 */}
-      {Object.keys(supportContent.comments).length !== 0 && (
-        <S.CommentContainer>
-          <S.ProfileImgContainer>
-            <ProfileImg width={3} src={pageOwner.profileImgPath} />
-          </S.ProfileImgContainer>
-          <div>
-            <S.Nickname>{pageOwner.nickname}</S.Nickname>
-            <S.Comment>{supportContent.comments.content}</S.Comment>
-          </div>
-        </S.CommentContainer>
-      )}
-      <S.Line />
-      {isShowContractModal && (
-        <ContractModal
-          handleSetShowModal={setIsShowContractModal}
-          supportContent={supportContent}
-        />
-      )}
-    </div>
-  );
+  return <div></div>;
+
+  // return (
+  //   <div>
+  //     누가 누구에게 어떠한 후원을 했는지 노출
+  //     <S.Container>
+  //       <S.RepresentContainer
+  //         onClick={() => {
+  //           setIsShowContractModal(true);
+  //         }}
+  //       >
+  //         <S.ProfileImgContainer
+  //           onClick={(e) => {
+  //             e.stopPropagation();
+  //           }}
+  //         >
+  //           <ProfileImg
+  //             width={3}
+  //             src={supportContent.fromMember.profileImgPath}
+  //             to={`/${supportContent.fromMember.pageName}`}
+  //           />
+  //         </S.ProfileImgContainer>
+  //         <S.TitleWrapper>
+  //           <S.TitleContent>
+  //             <S.Nickname>{supportContent.fromMember.nickname}</S.Nickname>
+  //             &nbsp;
+  //             {supportText}
+  //             &nbsp;
+  //             <S.Nickname>{pageOwner.nickname}</S.Nickname>
+  //           </S.TitleContent>
+  //           {isOwner && Object.keys(supportContent.comments).length === 0 && (
+  //             <S.ReplyBtn
+  //               onClick={(e) => {
+  //                 e.stopPropagation();
+  //                 setIsShowReplyInput((prev) => !prev);
+  //               }}
+  //             >
+  //               {isShowReplyInput ? "Close" : "Reply"}
+  //             </S.ReplyBtn>
+  //           )}
+  //         </S.TitleWrapper>
+  //         <S.EmojiWrapper>
+  //           <label style={{ marginLeft: "0.5rem" }}>{emoji}</label>
+  //         </S.EmojiWrapper>
+  //       </S.RepresentContainer>
+  //       {isOwner && isShowReplyInput && (
+  //         <S.InputContainer>
+  //           <S.ReplyInput handleOnChangeValue={handleOnChangeInput} />
+  //           <S.RegistBtn onClick={doRegistComment}>Regist</S.RegistBtn>
+  //         </S.InputContainer>
+  //       )}
+  //     </S.Container>
+  //     {/* 서포트 메세지가 있을 경우 노출 */}
+  //     {supportContent.sendMsg !== "" && (
+  //       <S.CommentContainer>
+  //         <S.ProfileImgContainer>
+  //           <ProfileImg
+  //             width={3}
+  //             src={supportContent.fromMember.profileImgPath}
+  //             to={`/${supportContent.fromMember.pageName}`}
+  //           />
+  //         </S.ProfileImgContainer>
+  //         <div>
+  //           <S.Nickname>{supportContent.fromMember.nickname}</S.Nickname>
+  //           <S.Comment>{supportContent.sendMsg}</S.Comment>
+  //           <S.SupportMsgText>Support message</S.SupportMsgText>
+  //         </div>
+  //       </S.CommentContainer>
+  //     )}
+  //     {/* 해당 후원에 댓글이 있을 경우 노출 */}
+  //     {Object.keys(supportContent.comments).length !== 0 && (
+  //       <S.CommentContainer>
+  //         <S.ProfileImgContainer>
+  //           <ProfileImg width={3} src={pageOwner.profileImgPath} />
+  //         </S.ProfileImgContainer>
+  //         <div>
+  //           <S.Nickname>{pageOwner.nickname}</S.Nickname>
+  //           <S.Comment>{supportContent.comments.content}</S.Comment>
+  //         </div>
+  //       </S.CommentContainer>
+  //     )}
+  //     <S.Line />
+  //     {isShowContractModal && (
+  //       <ContractModal
+  //         handleSetShowModal={setIsShowContractModal}
+  //         supportContent={supportContent}
+  //       />
+  //     )}
+  //   </div>
+  // );
 };
 
 export default RecentSupportBlock;
@@ -168,4 +144,5 @@ RecentSupportBlock.propTypes = {
     }).isRequired,
     sendMsg: PropTypes.string,
   }).isRequired,
+  isOwner: PropTypes.bool,
 };
