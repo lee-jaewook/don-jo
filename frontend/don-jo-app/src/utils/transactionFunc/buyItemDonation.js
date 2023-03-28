9ea56ecc528894c712d980e61b020c7722338c51
Hyunsu <83157338+hyunssu@users.noreply.github.com>
2023-03-28 11:07:53 +0900

    :zap: update : 아이템 구매 후원 함수 API 연결
    .

--- frontend/don-jo-app/src/utils/transactionFunc/buyItemDonation.js
+++ frontend/don-jo-app/src/utils/transactionFunc/buyItemDonation.js
@@ -3,7 +3,7 @@ import ApplicationHandler from "../../contracts/ApplicationHandler.json";
 import { supportApi } from "../../api/support";
 import { isMobile } from "react-device-detect";
 
-export const buyItemDonation = ({ itemId }) => {
+export const buyItemDonation = (item) => {
   // 모바일 여부 확인
   if (!isMobile) {
     // 메타마스크 설치 여부 확인
@@ -21,44 +21,61 @@ export const buyItemDonation = ({ itemId }) => {
             );
             web3.setProvider(infuraWeb3.currentProvider);
             // const address = "0x6c3ea1dD30BEb9B449272d393693A47727a5dF12";
-            const valueInWei = web3.utils.toWei("0.000003", "ether");
-
+            const valueInWei = web3.utils.toWei(
+              (item.price * Math.pow(10, -3)).toString(),
+              "ether"
+            );
+            // const valueInWei = item.price;
+            console.log("valueInWei: ", valueInWei);
             // const myWallet = web3.walletAddress;
             const myContract = new web3.eth.Contract(
               ApplicationHandler.abi, // abi 설정
               "0xc45694392A301B63a1FD0A1b2762521915a78f44" // contract 주소
             );
 
-            const tx = myContract.methods.buyItemDonation(
-              "0x6c3ea1dD30BEb9B449272d393693A47727a5dF12",
-              itemId
-            );
+            const tx = myContract.methods.buyItemDonation(item.seller, item.id);
+
             window.ethereum
               .request({
                 method: "eth_sendTransaction",
                 params: [
                   {
                     from: accounts[0],
-                    to: "0x6c3ea1dD30BEb9B449272d393693A47727a5dF12",
-                    value: valueInWei,
-                    gas: "20000",
+                    to: item.seller,
+                    value: valueInWei.toString(),
+                    // gas: "100000000000000000",
                     data: tx.encodeABI(),
                   },
                 ],
               })
-              .then((res) => {
-                console.log("transaction 성공");
-                console.log("res: ", res);
-                // const donationDto = {
-                //   amountEth: 0,
-                //   fromAddress: myWallet,
-                //   sendMsg: "string",
-                //   supportType: "donation",
-                //   supportUid: 0,
-                //   toAddress: "string",
-                //   transactionHash: res,
-                // };
-                // saveDonation(donationDto);
+              .then((txHash) => {
+                const receiptPromise = new Promise(function (resolve, reject) {
+                  const intervalId = setInterval(function () {
+                    web3.eth.getTransactionReceipt(txHash).then((receipt) => {
+                      if (receipt !== undefined && receipt !== null) {
+                        console.log("야옹야옹!");
+                        clearInterval(intervalId);
+                        resolve({ receipt, txHash });
+                      }
+                    });
+                  }, 1000);
+                });
+                return receiptPromise;
+              })
+              .then(({ receipt, txHash }) => {
+                console.log("Transaction successful");
+                console.log("receipt: ", receipt);
+                const donationDto = {
+                  amountEth: item.price,
+                  fromAddress: accounts[0],
+                  sendMsg: item.message,
+                  supportType: "item",
+                  supportTypeUid: item.id,
+                  supportUid: "0",
+                  toAddress: item.seller,
+                  transactionHash: txHash,
+                };
+                saveDonation(donationDto);
               })
               .catch((err) => console.log(err));
           });
