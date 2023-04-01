205e8eb4c8416e8f00614c696c5f017c0cb03d52
안태현 <dksxogus1012@gmail.com>
2023-04-01 20:23:58 +0900

    :recycle: refactor: 회원가입 처리 헤더에서 분리

--- frontend/don-jo-app/src/components/Common/Header/index.jsx
+++ frontend/don-jo-app/src/components/Common/Header/index.jsx
@@ -4,19 +4,12 @@ import { Link, useLocation } from "react-router-dom";
 import homeIcon from "../../../assets/img/common/home.png";
 import { useEffect, useState } from "react";
 import { metamaskLogIn } from "../../../utils/metamaskLogIn";
-import { walletConnectLogIn } from "../../../utils/walletConnectLogIn";
 import { useDispatch, useSelector } from "react-redux";
-import SignUp from "../../SignUp";
 import LogoImg from "../../../assets/img/common/app-logo.svg";
 import { FiExternalLink } from "@react-icons/all-files/fi/FiExternalLink";
-import PasswordSetModal from "../Modal/PasswordSetModal";
-import { checkSignUpValidation } from "../../../utils/validation/checkSignUpValidation";
-import { memberApi } from "../../../api/member";
 import SelectBox from "./SelectBox";
-import { fileApi } from "../../../api/file";
 import WalletConnectLogin from "../WalletConnectLogin";
-
-const IMAGE_TYPE = "img/item";
+import SignUp from "../SignUp";
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -26,22 +19,11 @@ const Header = () => {
   );
 
   const isLogin = useSelector((state) => state.member.isLogIn);
-  const address = useSelector((state) => state.member.walletAddress);
   const location = useLocation();
   const [profileImgSrc, setProfileImgSrc] = useState("");
   const [profileLinkTo, setProfileLinkTo] = useState("");
-  const [isShowSignUpModal, setIsShowSignUpModal] = useState(false);
+  const [isShowSignUp, setIsShowSignUp] = useState(false);
   const [isLocalSrc, setIsLocalSrc] = useState(false);
-  const [isShowPasswordSetModal, setIsShowPasswordSetModal] = useState(false);
-  const [userInfo, setUserInfo] = useState({
-    nickName: "",
-    pageName: "",
-    password: "",
-  });
-  const [profileImgPath, setProfileImgPath] = useState({
-    previewImgUrl: "",
-    file: {},
-  });
 
   useEffect(() => {
     setProfileImgSrc(profileImagePath);
@@ -59,30 +41,6 @@ const Header = () => {
     }
   }, [location.pathname]);
 
-  const handleSignUpModalOpen = () => {
-    setIsShowSignUpModal(false);
-  };
-
-  /**
-   * isModalOpen - Signup 모달 함수
-   * 설명:
-   * SignUp 모달에서 닫기 버튼 클릭 시
-   * state 초기화 및 모달 닫기 함수 실행
-   */
-  const isModalOpen = () => {
-    setUserInfo({
-      nickName: "",
-      pageName: "",
-      password: "",
-    });
-    setProfileImgPath({
-      previewImgUrl: "",
-      file: {},
-    });
-    setIsShowSignUpModal(false);
-    setIsShowPasswordSetModal(false);
-  };
-
   /**
    * handleMetamaskLogInClick - 메타마스크 LogIn 함수
    * 설명:
@@ -94,104 +52,9 @@ const Header = () => {
     metamaskLogIn({
       dispatch,
       handleModalOpen: () => {
-        setIsShowSignUpModal(true);
+        setIsShowSignUp(true);
       },
     });
-  };
-
-  /**
-   * handleContinueButtonClick
-   * 설명:
-   * SignUp Modal (회원가입 1단계)에서 Continue 버튼 클릭 시 호출
-   * nickName, pageName의 Validation을 체크
-   * 유효할 경우, SignUp Modal에서 Password Modal로 이동.
-   * 유효하지 않을 경우, alert를 띄우고, SignUp Modal 유지.
-   */
-  const handleContinueButtonClick = async () => {
-    if (!checkSignUpValidation(userInfo.nickName, userInfo.pageName)) return;
-    await memberApi
-      .checkPageName(userInfo.pageName)
-      .then(() => {
-        setIsShowSignUpModal(false);
-        setIsShowPasswordSetModal(true);
-      })
-      .catch(({ response: { status } }) => {
-        if (status === 409) {
-          // sendToastMessage("🚫 Please enter a message");
-          alert("중복된 닉네임입니다.");
-        }
-      });
-  };
-
-  /**
-   * doSignUp - 회원가입 함수
-   * 설명:
-   *
-   */
-
-  const doSignUp = async (inputPassword) => {
-    let signUpMemberCond = {
-      ...userInfo,
-      nickname: userInfo.nickName,
-      address: address,
-      password: inputPassword,
-      profileImgPath: "",
-    };
-    // 아이템 이미지 업로드 확인
-    if (profileImgPath.previewImgUrl !== "") {
-      let createdItemPath = await handleUploadFile(
-        profileImgPath.file,
-        IMAGE_TYPE
-      );
-      signUpMemberCond = {
-        ...signUpMemberCond,
-        profileImgPath: createdItemPath,
-      };
-    }
-    console.log("signUpMemberCond: ", signUpMemberCond);
-    //회원가입하는 함수
-    memberApi
-      .signUp(signUpMemberCond)
-      .then((res) => {
-        metamaskLogIn({ dispatch, handleModalOpen: isModalOpen });
-      })
-      .catch((error) => {
-        console.log("회원가입 실패");
-      });
-  };
-
-  /**
-   * handleWalletConnectLogInClick - 월렛커넥트 LogIn 함수
-   * 설명:
-   * 회원일 경우, 로그인 처리
-   * 비회원일 경우, 회원가입 모달 띄우기
-   */
-  const handleWalletConnectLogInClick = () => {
-    walletConnectLogIn({
-      dispatch,
-      handleModalOpen: () => {
-        setIsShowSignUpModal(true);
-      },
-    });
-  };
-
-  const handleSetPassword = (inputPassword) => {
-    setUserInfo((prev) => ({
-      ...prev,
-      password: inputPassword,
-    }));
-  };
-
-  const handleUploadFile = async (file, type) => {
-    const formData = new FormData();
-    formData.append("multipartFile", file);
-
-    try {
-      const { data } = await fileApi.uploadFile(formData, type);
-      return data;
-    } catch (error) {
-      console.log("error: ", error);
-    }
   };
 
   return (
@@ -231,23 +94,8 @@ const Header = () => {
           )}
         </S.ProfileImgContainer>
       </S.Header>
-      {/* 임시로 FullScreen 모달 띄우기 -> 로그인 모달로 바뀔 예정 */}
-      {isShowSignUpModal && (
-        <SignUp
-          isModelOpen={isModalOpen}
-          userInfo={userInfo}
-          setUserInfo={setUserInfo}
-          handleContinueButtonClick={handleContinueButtonClick}
-          profileImgPath={profileImgPath}
-          setProfileImgPath={setProfileImgPath}
-        />
-      )}
-      {isShowPasswordSetModal && (
-        <PasswordSetModal
-          handleSetShowModal={isModalOpen}
-          setPassword={handleSetPassword}
-          doSignUp={doSignUp}
-        />
+      {isShowSignUp && (
+        <SignUp isShowSignUp={isShowSignUp} setIsShowSignUp={setIsShowSignUp} />
       )}
     </S.HeaderContainer>
   );
