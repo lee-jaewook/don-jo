80ceb66365cde6d0776af2d3aa09d22c7c9218da
강태선 <ts7681@naver.com>
2023-04-04 23:31:26 +0900

    :bug: fix: 위시리스트 후원 기능 방식 변경

--- frontend/don-jo-app/src/index.js
+++ frontend/don-jo-app/src/index.js
@@ -5,34 +5,8 @@ import store from "./stores/store";
 import { Provider } from "react-redux";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
-import {
-  w3mConnectors,
-  w3mProvider,
-} from "@web3modal/ethereum";
-import { configureChains, createClient, WagmiConfig } from "wagmi";
-import {
-  //mainnet,
-  polygonMumbai,
-  // polygon,
-  // sepolia,
-} from "wagmi/chains";
-
-const projectId = "bff442f05c60a67faf40efa21f494f0d";
-
-// 2. Configure wagmi client
-const chains = [
-  //mainnet,
-  polygonMumbai
-  // polygon,
-];
-
-const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
-
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors: w3mConnectors({ version: 1, chains, projectId }),
-  provider,
-});
+import { WagmiConfig } from "wagmi";
+import { wagmiClient } from "./api/wagmi/walletConnectSetting";
 
 const persistor = persistStore(store);
 
