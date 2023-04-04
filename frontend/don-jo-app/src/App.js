fcd1813c4b6e5f0ae5a783da85ac5ddccf006c2e
송현주 <shyunju7@naver.com>
2023-04-04 17:24:51 +0900

    ✨ feat: toast message css 설정 추가

--- frontend/don-jo-app/src/App.js
+++ frontend/don-jo-app/src/App.js
@@ -1,6 +1,7 @@
 import GlobalStyle from "./styles/GlobalStyles";
 import AppRouter from "./AppRouter";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 // 모바일 웹에서 올바른 영역을 잡기 위한 작업
 let vh = window.innerHeight * 0.01;
