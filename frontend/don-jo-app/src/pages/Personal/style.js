5b79e979450f35ce913061602ba7576577db7e88
안태현 <dksxogus1012@gmail.com>
2023-04-04 15:38:40 +0900

    :bug: fix: 로그인 됐을 시 페이지 유저 체크 & 자기소개 없을 시 미노출

--- frontend/don-jo-app/src/pages/Personal/style.js
+++ frontend/don-jo-app/src/pages/Personal/style.js
@@ -142,6 +142,7 @@ export const IntroductionContainer = styled.div`
   padding: 1rem 0 1rem 1rem;
   border: 1px solid var(--color-text-secondary);
   border-radius: 1.25rem;
+  display: ${(props) => (props.isShow ? "none" : "block")};
 `;
 
 export const IntroductionEdit = styled.div`
