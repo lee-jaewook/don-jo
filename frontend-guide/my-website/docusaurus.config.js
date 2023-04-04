1a8102c48e9d2815927fb9888aaf2ad293adb5ae
이재욱 <jaewook_lee@outlook.com>
2023-04-04 09:30:53 +0900

    :art: fix: 로고 변경

--- frontend-guide/my-website/docusaurus.config.js
+++ frontend-guide/my-website/docusaurus.config.js
@@ -67,7 +67,7 @@ const config = {
         title: "",
         logo: {
           alt: "My Site Logo",
-          src: "img/logo.svg",
+          src: "img/logo2.svg",
         },
         items: [
           {
