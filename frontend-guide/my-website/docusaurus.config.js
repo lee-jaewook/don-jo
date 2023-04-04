94584acfb20f0042800e729e57442fa148594d3d
이재욱 <jaewook_lee@outlook.com>
2023-04-04 09:51:20 +0900

    :art: fix: 다크 모드 제거

--- frontend-guide/my-website/docusaurus.config.js
+++ frontend-guide/my-website/docusaurus.config.js
@@ -62,6 +62,11 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
+      colorMode: {
+        defaultMode: "light",
+        disableSwitch: true,
+      },
+
       image: "img/docusaurus-social-card.jpg",
       navbar: {
         title: "",
